8ce9bdc077ee107607379801f63d1d8d6c6bcb0c
pawgid <pawelgidaszewski@iMac-Pawe.local>
2019-10-21 22:00:29 +0200

    Added ServerComponent and styling to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { ServerComponent } from './servers/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleFormComponent
+    SimpleFormComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
