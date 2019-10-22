29ab028e0c97f527f35898d439cec0f4336bf3e3
pawgid <pawelgidaszewski@iMac-Pawe.local>
2019-10-22 20:28:27 +0200

    Added another component and bootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
 import { ServerComponent } from './servers/server.component';
+import { SubServerComponent } from './sub-server/sub-server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleFormComponent,
-    ServerComponent
+    ServerComponent,
+    SubServerComponent
   ],
   imports: [
     BrowserModule,
